de53df541e2790a977fbe501b4e5df396ce620f9
choi-ys <project.log.062@gmail.com>
2025-02-14 16:13:25 +0900

    initial commit

---
+++ src/main/java/com/tutorial/team/WooahanEatsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooahanEatsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WooahanEatsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorial/team/WooahanEatsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooahanEatsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
